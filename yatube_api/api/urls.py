7b6d79c38a0aeb72d009bf39806202077656e1a1
Екатерина Чигина <ekaterinachigina2004@gmail.com>
2023-05-12 21:08:59 +0300

    first commit

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
+router.register(r'^posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comment')
+
+
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
+]
---
+++ yatube_api/api/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from api.permission import IsOwnerOrReadOnly
+from api.serializers import PostSerializer, GroupSerializer, CommentSerializer
+from posts.models import Post, Group
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('pub_date')
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all().order_by('title')
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        return post.comments
